4da72c83d8edc4bae77e209682c39237d4a52e88
whimsicorn <agalecka90@gmail.com>
2022-02-18 14:55:26 +0100

    add model films

--- app_oscars/admin.py
+++ app_oscars/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Films
 
-# Register your models here.
+admin.site.register(Films)
--- app_oscars/models.py
+++ app_oscars/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Films(models.Model):
+    title = models.CharField(max_length=30, null=True, blank=True)
+    director = models.CharField(max_length=30, null=True, blank=True)
+    year = models.SmallIntegerField(null=True, blank=True)
+    country = models.CharField(max_length=20, null=True, blank=True)
+    description = models.CharField(max_length=200,null=True, blank=True)
+    poster = models.CharField(max_length=20, null=True, blank=True)
