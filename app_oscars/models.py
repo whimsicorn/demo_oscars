c69a6f63750f59e66de5f1ee47a82f5b6d1abeab
whimsicorn <agalecka90@gmail.com>
2022-02-18 19:00:51 +0100

    finish model films

--- app_oscars/models.py
+++ app_oscars/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 class Films(models.Model):
-    title = models.CharField(max_length=30, null=True, blank=True)
-    director = models.CharField(max_length=30, null=True, blank=True)
-    year = models.SmallIntegerField(null=True, blank=True)
-    country = models.CharField(max_length=20, null=True, blank=True)
-    description = models.CharField(max_length=200,null=True, blank=True)
-    poster = models.CharField(max_length=20, null=True, blank=True)
+    COUNTRIES = [
+        ('US','Stany Zjednoczone'),
+        ('GB',"Wielka Brytania"),
+        ('JP','Japonia'),
+    ]
+    title = models.CharField(max_length=40,default="", blank=True)
+    polish_title = models.CharField(max_length=40,default="", blank=True)
+    director = models.CharField(max_length=30,default="", blank=True)
+    year = models.PositiveSmallIntegerField(null=True, blank=True)
+    country = models.CharField(max_length=2, choices=COUNTRIES, default='US')
+    description = models.TextField(max_length=500,default="Film oskarowy", blank=True)
+    poster = models.ImageField(upload_to='posters', null=True,blank=True)
+
+    def __str__(self):
+        return self.title+", "+self.director
