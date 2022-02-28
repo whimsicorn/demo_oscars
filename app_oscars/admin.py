b1a1749fa5f4412dc20f8da865e4cdfb4a7280df
whimsicorn <agalecka90@gmail.com>
2022-02-28 17:56:10 +0100

    make django admin section

--- app_oscars/admin.py
+++ app_oscars/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Films
 
-admin.site.register(Films)
+@admin.register(Films)
+class FilmAdmin (admin.ModelAdmin):
+    list_display = ['title','director','country']
+    list_filter = ['country']
+    search_fields=['title','description']
+
